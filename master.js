5206182dd5670fda8e12ad22a77ec6dd64c31aaf
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2020-02-25 14:50:51 +0100

    Add files via upload

---
+++ master.js
@@ -0,0 +1,25 @@
+let q1 = $("#question1").addClass("answer");
+
+let q2 = $(".question2").addClass("answer");
+
+let q3 = $(".question3 span").addClass("answer");
+
+let q4 = $(".question4 [data-answer]").addClass("answer");
+
+let q5 = $(".question5 div div div").addClass("answer");
+
+let q6 = $(".question6 [data-answer]").addClass("answer");
+
+let q7 = $(".question7 [data-answer]").addClass("answer");
+
+let q8 = $(".question8 ul li:first").addClass("answer");
+
+let q9 = $(".question9 ul li:last").addClass("answer");
+
+let q10 = $(".question10 ul li:last a").addClass("answer");
+
+let q11 = $(".question11 a[href=\"#\"]").addClass("answer");
+
+let q12 = $(".question12 a[href^=\"http:\"]").addClass("answer");
+
+let q13 = $(".question13 a[href!=\"#\"]").addClass("answer");
